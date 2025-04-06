69372e14be13d8cf8f63b52475654b59b802c213
gustablo <gpinheiro2001@gmail.com>
2025-04-06 01:31:38 -0300

    add followers count

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :likes
 
   has_many :following, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
@@ -13,5 +14,7 @@ class User < ApplicationRecord
 
   def as_json(options = {})
     super(only: %i[ id email_address nickname ])
+      .merge(following_count: following.count)
+      .merge(followers_count: followers.count)
   end
 end
