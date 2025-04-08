eb7f5bf8a8a788883600ccfd0fefaf5063e55cb5
gustablo <gpinheiro2001@gmail.com>
2025-04-07 23:57:34 -0300

    followers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post "signup" => "users#create"
   resources :users, only: %i[ show index ] do
     get "/posts" => "users#posts"
+    get "/followers" => "users#followers"
   end
 
   resources :follows
