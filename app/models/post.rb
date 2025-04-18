cbd1e076b1f2d2f869991bff563437d45111f1a4
Gustavo Pinheiro <43258062+gustablo@users.noreply.github.com>
2025-04-18 01:47:01 -0300

    push again

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
   has_many_attached :images
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_one :parent_post, class_name: "Post", foreign_key: "parent_post_id"
+  belongs_to :parent_post, class_name: "Post"
+  has_many :childrens, class_name: "Post", foreign_key: "parent_post_id"
   include Likeable
   include Shareable
   include Commentable
